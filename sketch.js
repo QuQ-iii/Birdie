af3cd1b72e8c88b8e83c487f0200a7401ad0d06b
QuQ-iii <75155537+QuQ-iii@users.noreply.github.com>
2020-12-10 17:04:19 +0530

    Update sketch.js
    made the restart button clickable

--- sketch.js
+++ sketch.js
@@ -134,7 +134,7 @@ function draw() {
     branchGroup.setLifetimeEach(-1);
     slimeGroup.setLifetimeEach(-1);
     
-    if(mousePressedOver(restart)) {
+    if(mousePressedOver(restart)|| touches.length > 0) {
       
         gameState = PLAY;
         score = 0;
